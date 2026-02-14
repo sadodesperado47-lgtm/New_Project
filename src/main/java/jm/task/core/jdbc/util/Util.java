a7981eba09b8a807c145dec5289444ac8fa86ffd
Andrey T. <troshin_a_a@mail.ru>
2026-02-14 10:51:19 +0300

    connect_bd

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,44 @@
 package jm.task.core.jdbc.util;
 
+import com.mysql.cj.jdbc.Driver;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private Connection connect;
+
+    private final String url = "jdbc:mysql://localhost:3306/test_db";
+    private final String user = "root";
+    private final String password = "root";
+
+    private Util() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            this.connect = DriverManager.getConnection(url, user, password);
+        } catch (ClassNotFoundException e) {
+            throw  new SQLException(e);
+        }
+    }
+
+    private static  class Holder {
+        private static final Util INSTANCE;
+        static  {
+            try {
+                INSTANCE = new Util();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+    }
+
+    public static  Util getInstance() {
+        return Holder.INSTANCE;
+    }
+
+    public Connection getConnect() {
+        return connect;
+    }
 }
