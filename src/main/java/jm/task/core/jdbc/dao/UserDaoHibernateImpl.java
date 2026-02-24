cef2b337d477712501018ebaf0d746be4c67c736
Andrey T. <troshin_a_a@mail.ru>
2026-02-24 18:56:08 +0300

    CRUD_DAO_Hiber_2.0

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -5,6 +5,7 @@ import jm.task.core.jdbc.util.Util;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
+
 import java.util.Collections;
 import java.util.List;
 
@@ -15,11 +16,28 @@ public class UserDaoHibernateImpl implements UserDao {
     }
     @Override
     public void createUsersTable() {
+        try(Session session = Util.getSession()) {
+            Transaction tx = session.beginTransaction();
+            session.createNativeQuery("CREATE TABLE IF NOT EXISTS users (" +
+                    "id BIGINT PRIMARY KEY AUTO_INCREMENT, " +
+                    "name VARCHAR(50), " + "lastName VARCHAR(50)," +
+                    "age TINYINT)").executeUpdate();
+            tx.commit();
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 
     @Override
     public void dropUsersTable() {
+        try (Session session = Util.getSession()) {
+            Transaction tx = session.beginTransaction();
+            session.createNativeQuery("DROP TABLE IF EXISTS users").executeUpdate();
+            tx.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 
